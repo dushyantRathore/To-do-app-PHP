e7f28879ee491e6fa1cc5797471098198903c84c
Dushyant Rathore <dushyant.bgs@gmail.com>
2016-06-19 13:55:50 +0530

    Add files via upload

---
+++ renew_task.php
@@ -0,0 +1,19 @@
+<?php 
+
+require("config.php");
+
+if(isset($_GET['id']))
+{
+	$id = $_GET['id'];
+
+	if(!empty($id))
+	{
+
+		$sql = "UPDATE items SET done = 0 WHERE id = $id";
+		$query_run = mysql_query($sql);
+		header('Location: ' . $_SERVER['HTTP_REFERER']);
+		exit;
+	}
+}
+
+?>
